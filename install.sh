a4474a76de916c64b60a8b0bef79ab839db0f7a9
ws.jung.d <ws.jung.d@gmail.com>
2018-07-10 08:16:47 +0900

    add mpd, i3 configurations

---
+++ install.sh
@@ -0,0 +1,14 @@
+pacman -S terminator --noconfirm
+pacman -S ack --noconfirm
+pacman -S tree --noconfirm
+pacman -S vim --noconfirm
+
+
+
+sudo pacman -S mpd
+sudo pacman -S ncmpcpp
+ln -s ~/dotfiles/mpd ~/.config/mpd
+
+
+yaourt -S i3-kde --noconfirm
+ln -s ~/dotfiles/i3 ~/.config/i3
